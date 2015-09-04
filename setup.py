d6afdabd8ff034ccbc4eb23df815cccc3e589f94
rectangletangle <rectangletangle@gmail.com>
2015-09-03 17:45:00 -0700

    pkg info

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+version = '0.0.0'
+
+urltemplate = 'https://github.com/rectangletangle/dunderscore/tarball/{version}'
+
+setup(name='dunderscore',
+      packages=['dunderscore'],
+      version=version,
+      description='Useful functions for Python 2 or 3',
+      author='Drew French',
+      author_email='rectangletangle@gmail.com',
+      url='https://github.com/rectangletangle/dunderscore',
+      download_url=urltemplate.format(version=version))
