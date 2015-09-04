8ef1c2c404c6e2c626f0f675607cd459500bb83b
rectangletangle <rectangletangle@gmail.com>
2015-09-03 18:26:38 -0700

    fixd import

--- dunderscore/__init__.py
+++ dunderscore/__init__.py
@@ -1,6 +1,6 @@
 """ Dunderscore, useful functions for Python 2 or 3 """
 
-from . import (chunked,
-               overlapped,
-               grouped,
-               pluralized)
+from .__ import (chunked,
+                 overlapped,
+                 grouped,
+                 pluralized)
